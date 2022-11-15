5cb82e43f51ffe307266de8bab45af789e2455f8
thismakerx <118231373+thismakerx@users.noreply.github.com>
2022-11-15 12:56:13 +1030

    EDIT: Updated title block
    Tidied up title block

--- heart-beat.ino
+++ heart-beat.ino
@@ -1,12 +1,14 @@
 /*********************************************
-  Heart Beat Example
-  This Maker
-  2-Sep-2022
- 
+     Project: Real Time Heart Beat LED
+      Author: This Maker
+   Start Rev: 2-Sep-2022
+  Latest Rev: 15-Nov-2022
+  
   Adds a real time heartbeat pattern to an
   LED to show that the code on the device is
   still being run ie not stuck in an endless
   loop, or frozen.
+  
 **********************************************/
 
 #define HBLED LED_BUILTIN
