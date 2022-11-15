2b88e789ea1b972505feef3b04a0ad9939eeb2d4
thismakerx <118231373+thismakerx@users.noreply.github.com>
2022-11-15 12:50:46 +1030

    EDIT: Added comments
    Added a comment to explain the need for Arduino.h

--- functions.h
+++ functions.h
@@ -1,7 +1,7 @@
 #ifndef FUNCTIONS_H
   #define FUNCTIONS_H
 
-  #include <Arduino.h>
+  #include <Arduino.h>  // This library is needed in order to use the byte datatype.
 
   void heartBeat(byte pin);
 
